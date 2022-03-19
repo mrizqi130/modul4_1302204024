7b86b2effc0476c9850c95ff87954e8b6b1b2396
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2022-03-19 15:30:40 +0800

    menambahkan class SimpleDataBase v0.1

--- modul4_1302204024/Program.cs
+++ modul4_1302204024/Program.cs
@@ -12,4 +12,29 @@ namespace modul4_1302204024
         {
         }
     }
+
+    public class SimpleDataBase<T, Date>
+    {
+        private List<T> storedData;
+        private List<Date> inputDates;
+
+        public SimpleDataBase()
+        {
+
+        }
+
+        public void AddNewData(T data)
+        {
+            dynamic data1;
+            data1 = data;
+            this.storedData = data1;
+            data1 = DateTime.Now;
+            this.inputDates = data1;
+        }
+
+        public void PrintAllData()
+        {
+
+        }
+    }
 }
